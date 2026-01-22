4c9c73ea977bffc5e4227e55d628b574a0fb0c44
YashrajCodex <yashrajai98@gmail.com>
2026-01-22 23:49:51 +0530

    updated project folder

--- Project-32/Angular_Todolist/cwh-todo-list/src/app/features/todos/todos.component.ts
+++ Project-32/Angular_Todolist/cwh-todo-list/src/app/features/todos/todos.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from './todo';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule, Check, Repeat, Trash } from 'lucide-angular';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { FormsModule } from '@angular/forms';
+
+const STORAGE_KEY = 'stored_todos'
 
 @Component({
   selector: 'app-todos',
@@ -12,39 +14,51 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css',
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+
   readonly icons = { Check, Repeat, Trash };
   title: string = '';
   description: string = '';
-  todos: Todo[] = [
-    {
-      sno: 1,
-      title: 'Learn Angular',
-      desc: 'Stop fighting the framework',
-      active: true,
-    },
-    {
-      sno: 2,
-      title: 'Build Todo App',
-      desc: 'Use Standalone components property',
-      active: true,
-    },
-    {
-      sno: 3,
-      title: 'Dominate',
-      desc: 'React + Angular brain unlocked',
-      active: false,
-    },
-  ];
+  todos: Todo[] = [];
+  // {
+  //   sno: 1,
+  //   title: 'Learn Angular',
+  //   desc: 'Stop fighting the framework',
+  //   active: true,
+  // },
+  // {
+  //   sno: 2,
+  //   title: 'Build Todo App',
+  //   desc: 'Use Standalone components property',
+  //   active: true,
+  // },
+  // {
+  //   sno: 3,
+  //   title: 'Dominate',
+  //   desc: 'React + Angular brain unlocked',
+  //   active: false,
+  // },
 
+  ngOnInit(): void {
+    const savedTodos = localStorage.getItem(STORAGE_KEY);
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  }
+
+  saveTodos() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+  }
   handleDelete(todo: Todo) {
     this.todos = this.todos.filter((t) => t.sno !== todo.sno);
-    console.log(this.todos.length)
+    this.saveTodos();
+    // console.log(this.todos.length)
   }
   handleToggle(todo: Todo) {
     this.todos = this.todos.map((t) =>
       t.sno === todo.sno ? { ...t, active: !t.active } : t,
     );
+    this.saveTodos();
   }
   handleAddTodo() {
     const length = this.todos?.length;
@@ -57,5 +71,8 @@ export class TodoComponent {
     };
     // console.log(todoObject);
     this.todos.push(todoObject);
+
+    this.saveTodos();
   }
+  
 }
